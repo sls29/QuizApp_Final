45e3144253d39836ae6d767785be50fa369d21e2
roger <seles.lucian@gmail.com>
2023-07-24 11:04:52 +0300

    class

--- src/main/java/com/QuizApp/model/User.java
+++ src/main/java/com/QuizApp/model/User.java
@@ -1,4 +1,40 @@
 package com.QuizApp.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table (name = "users")
+@NoArgsConstructor
+
 public class User {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String passwordHash;
+    private Date registeredAt;
+    private Date lastLogin;
+
+    public User (String firstName, String lastName, String email,
+                 String passwordHash, Date registeredAt, Date lastLogin) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.passwordHash = passwordHash;
+        this.registeredAt = registeredAt;
+        this.lastLogin = lastLogin;
+    }
+
 }
