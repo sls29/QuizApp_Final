0fa4c5703853621ab46a67767e0de9587fa87071
roger <seles.lucian@gmail.com>
2023-07-24 11:32:21 +0300

    class

--- src/main/java/com/QuizApp/model/Question.java
+++ src/main/java/com/QuizApp/model/Question.java
@@ -1,4 +1,30 @@
 package com.QuizApp.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table (name = "questions")
+
 public class Question {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private int id;
+    private String type;
+    private int active;
+    private int level;
+    private int score;
+
+    public Question(String type, int active, int level, int score){
+        this.type = type;
+        this.active = active;
+        this.level = level;
+        this.score = score;
+    }
 }
--- src/main/java/com/QuizApp/model/Quiz.java
+++ src/main/java/com/QuizApp/model/Quiz.java
@@ -1,4 +1,32 @@
 package com.QuizApp.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "quiz")
+@NoArgsConstructor
+
 public class Quiz {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private int id;
+    private String title;
+    private String summary;
+    private String type;
+    private int score;
+
+    public Quiz(String title, String summary, String type, int score) {
+        this.title = title;
+        this.summary = summary;
+        this.type = type;
+        this.score = score;
+    }
 }
